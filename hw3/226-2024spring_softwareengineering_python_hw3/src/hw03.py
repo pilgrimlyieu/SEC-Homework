55369fdd400b7c792adb5a99df7604506c430929
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-04-17 21:52:50 +0800

    Add `hw3`

---
+++ hw3/226-2024spring_softwareengineering_python_hw3/src/hw03.py
@@ -0,0 +1,169 @@
+HW_SOURCE_FILE = 'hw03.py'
+
+#############
+# Questions #
+#############
+
+def num_sevens(x):
+    """Returns the number of times 7 appears as a digit of x.
+
+    >>> num_sevens(3)
+    0
+    >>> num_sevens(7)
+    1
+    >>> num_sevens(7777777)
+    7
+    >>> num_sevens(2637)
+    1
+    >>> num_sevens(76370)
+    2
+    >>> num_sevens(12345)
+    0
+    >>> from src.construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_sevens',
+    ...       ['Assign', 'AugAssign'])
+    True
+    """
+    return (x == 7) if x < 10 else (x % 10 == 7) + num_sevens(x // 10)
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from src.construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    def i(n):
+        return num_sevens(n) or (n % 7 == 0)
+    def r(n, p):
+        return 1 if n == 1 else r(n - 1, -p if i(n - 1) else p) + p
+    def g(n):
+        return 1 if n == 1 else (-1 if i(n - 1) else 1) * g(n - 1)
+    return r(n, g(n))
+
+def count_change(total):
+    """Return the number of ways to make change for total.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from src.construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    from math import log2
+    def c(t, m):
+        return 1 if t == 0 else 0 if t < 0 or m == 0 else c(t - m, m) + c(t, m // 2)
+    return c(total, 2 ** int(log2(total)))
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from src.construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    def m(n, d):
+        return 0 if n == 0 else ((d - n % 10 - 1) if (d - n % 10 > 1) else 0) + m(n // 10, n % 10)
+    return m(n, n % 10)
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    if n > 0:
+        move_stack(n - 1, start, 6 - start - end)
+        print_move(start, end)
+        move_stack(n - 1, 6 - start - end, end)
+
+
+###################
+# Extra Questions #
+###################
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from src.construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return (lambda f: lambda n: 1 if n == 0 else n * f(f)(n - 1))(lambda f: lambda n: 1 if n == 0 else n * f(f)(n - 1))
